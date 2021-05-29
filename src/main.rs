b3e9f3c51e9e456ff001b983bccff79e53f0ea25
Volksfest <s.barut@tu-bs.de>
2021-05-29 20:36:33 +0200

    Restructure and Rename functions
    WARNING: Not functional anymore!

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,58 @@ struct Context {
     stack : Vec<LeagueCommand>,
 }
 
+#[get("/")]
+async fn index(data: web::Data<Arc<Mutex<Context>>>) -> impl Responder {
+    let g = data.lock().unwrap();
+    HttpResponse::Ok()
+        .body(com::generator::create_html(&g.league))
+}
+
+#[get("/get_token")]
+async fn get_token(ctx : web::Data<Arc<Mutex<Context>>>) -> impl Responder {
+    let g = ctx.lock().unwrap();
+    HttpResponse::Ok()
+        .body(serde_json::to_string(&Respond::Token(
+            g.stack.len()
+        )).unwrap())
+}
+
+
+#[post("/update")]
+async fn update(ctx : web::Data<Arc<Mutex<Context>>>, payload : web::Json<com::command::Request>) -> impl Responder {
+    let mut g = match ctx.lock() {
+        Ok(g) => g,
+        _ => {return HttpResponse::BadGateway().finish();}
+    };
+
+    if payload.token >= g.stack.len() || g.stack.len() == 0 {
+        return HttpResponse::Ok();
+    }
+
+    let mut update_list = UpdateArgs{
+        matches: Vec::new(),
+        table_dom: create_table(&g.league).print(),
+        processed: false
+    };
+
+    let mut idx_list = Vec::new();
+
+    for cmd in g.stack.split_at(payload.token).1 {
+        match cmd.get_match_idx(&g.league) {
+            None => continue,
+            Some(idx) => if !idx_list.contains(&idx) {
+                update_list.matches.push(UpdateMatchArgs {
+                    idx,
+                    dom: create_single_match(&g.league, idx).print(),
+                });
+                idx_list.push(idx);
+            }
+        }
+    }
+
+    return HttpResponse::Ok().json(Respond::Update(update_list));
+}
+
 #[post("/upload/{secret}")]
 async fn upload(path: web::Path<String>, mut payload: Multipart, ctx: web::Data<Arc<Mutex<Context>>>) -> Result<HttpResponse, Error> {
 
@@ -56,96 +108,6 @@ async fn upload(path: web::Path<String>, mut payload: Multipart, ctx: web::Data<
         }
     }
     Ok(HttpResponse::Ok().into())
-}
-
-#[get("/")]
-async fn single(data: web::Data<Arc<Mutex<Context>>>) -> impl Responder {
-    let g = data.lock().unwrap();
-    HttpResponse::Ok()
-        .body(com::generator::create_html(&g.league))
-}
-
-#[get("/get_token")]
-async fn get_token(ctx : web::Data<Arc<Mutex<Context>>>) -> impl Responder {
-    let g = ctx.lock().unwrap();
-    HttpResponse::Ok()
-        .body(serde_json::to_string(&Respond::Token(
-            g.stack.len()
-        )).unwrap())
-}
-
-fn create_response<T: Serialize>(payload : T) -> HttpResponse {
-    HttpResponse::Ok().json(payload)
-}
-
-fn create_diff_update(ctx : &Context, token: usize, updated : bool) -> Option<Respond> {
-    if token >= ctx.stack.len() || ctx.stack.len() == 0 {
-        return None;
-    }
-
-    let mut update = UpdateArgs{
-        matches: Vec::new(),
-        table_dom: create_table(&ctx.league).print(),
-        processed: updated
-    };
-
-    let mut idx_list = Vec::new();
-
-    for cmd in ctx.stack.split_at(token).1 {
-        match cmd.get_match_idx(&ctx.league) {
-            None => continue,
-            Some(idx) => if !idx_list.contains(&idx) {
-                update.matches.push(UpdateMatchArgs {
-                    idx,
-                    dom: create_single_match(&ctx.league, idx).print(),
-                });
-                idx_list.push(idx);
-            }
-        }
-    }
-
-    Some(Respond::Update(update))
-}
-
-#[post("/api")]
-async fn api(ctx : web::Data<Arc<Mutex<Context>>>, payload : web::Json<com::command::Request>) -> impl Responder {
-    println!("Got API call ({:?})", payload);
-
-    let mut g = match ctx.lock() {
-        Ok(g) => g,
-        _ => {return HttpResponse::BadGateway().finish();}
-    };
-
-    if payload.token != g.stack.len() {
-        return create_response(create_diff_update(&*g, payload.token,false));
-    }
-
-    let path = g.path.clone();
-
-    let league = &mut g.league;
-
-    let update = match &payload.cmd
-    {
-        LeagueCommand::AddGame(game) => {
-            league.add_game(&game)
-        }
-        LeagueCommand::RemoveGames(game) =>{
-            league.remove_game(&game)
-        }
-    };
-
-    match update {
-        Ok(_) => {
-            save(&path, &g.league);
-            g.stack.push(payload.cmd.clone());
-            create_response(create_diff_update(&*g, payload.token, true))
-        },
-        Err(e) => {
-            println!("{}", e);
-            create_response(Respond::Error(e))
-        }
-    }
-
 }
 
 #[derive(Clap)]
@@ -233,9 +195,9 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move ||
         App::new()
             .data(shared_context.clone())
-            .service(single)
-            .service(api)
+            .service(index)
             .service(get_token)
+            .service(update)
             .service(upload)
             .service(fs::Files::new("/resource", "./asset"))
     )
